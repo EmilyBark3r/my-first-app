9b9cb5fb391ea9649f70009a07e404cbf95312d0
BarkerEmily <emilybarker@byui.edu>
2024-01-15 09:45:20 -0600

    W02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // styleUrl: './app.component.css'
+  styles: [`
+  h3 {
+    color: dodgerblue;
+  }
+  `]
 })
 export class AppComponent {
-  name = 'Emily';
+  // name = 'Emily';
 }
